1b2ce03abecda4e5f42e9dcf5a6b66447c24d1fb
metzee47 <syllamouhamadou351@gmail.com>
2026-08-18 17:19:50 +0100

    fix (header.jsx): Header responsivity fixed

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -2,12 +2,17 @@ import Header from "./Header";
 // import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 import '@/styles/layout.css'
+import { useMenu } from "@/context/AppContext";
 
 export default function MainLayout({activeMenu, children}) {
+
+    const {isMenuOpen} = useMenu()
+    
+
     return (
         <div className="main-layout">
             <Header activeItem={activeMenu}/>
-            <main className="main">
+            <main className={`main ${isMenuOpen ? 'open' : 'false'}`}>
                 {children}
                 <Outlet />
             </main>
