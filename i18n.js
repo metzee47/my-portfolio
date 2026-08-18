a32f408dc1c86bc9211c6ae0ddf3f406c9712a9d
metzee47 <syllamouhamadou351@gmail.com>
2026-08-18 17:26:54 +0100

    chore(i18n.js): internationalisation package download, internationalisation file init

---
+++ i18n.js
@@ -0,0 +1,25 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import Backend from "i18next-http-backend";
+
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: "fr",
+    fallbackLng: "fr",
+    ns: [
+      "home",
+      "projects",
+      "expertise",
+      "contact",
+      "common",
+    ],
+    defaultNS: "common",
+    interpolation: { escapeValue: false },
+    backend: {
+      loadPath: "/locales/{{lng}}/{{ns}}.json"
+    }
+  });
+
+export default i18n;
