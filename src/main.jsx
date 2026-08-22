47c7ac28367079461bc12f960b1e98afcc8e5205
metzee47 <syllamouhamadou351@gmail.com>
2026-08-22 12:00:56 +0100

    chore : vercel analytics install & config

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import '@/styles/index.css'
 import App from './App.jsx'
 import '../i18n'
+import { Analytics } from '@vercel/analytics/react';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 )
